031db952e51db5610d4353852cc4682bc1746402
Rashnotech <rashnotech@gmail.com>
2023-08-28 22:42:35 +0000

    Branch to implement tasks

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - a function that inserts a node as the right
+ * child of another node
+ * @parent: a pointer to the node to insert the right child
+ * @value: an integer value to store in the new node
+ *
+ * Return: a new node pointer on success otherwise NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
