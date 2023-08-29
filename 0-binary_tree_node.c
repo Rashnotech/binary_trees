c4690d396ed0e19cdfd37286e2239eab15229f24
Rashnotech <rashnotech@gmail.com>
2023-08-29 05:18:15 +0000

    Revamped binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,14 +14,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!node)
 		return (NULL);
 	node->n = value;
+	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
-	if (parent)
-	{
-		if (value <= parent->n)
-			parent->left = node;
-		else
-			parent->right = node;
-	}
 	return (node);
 }
