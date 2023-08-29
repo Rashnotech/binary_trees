9977db270b9135e27280318be0bd97ca26f441a1
Rashnotech <rashnotech@gmail.com>
2023-08-29 05:05:43 +0000

    Reused create binary

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,11 +13,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(NULL, value);
 	if (!new_node)
-		return(NULL);
-	new_node->n = value;
-	new_node->left = new_node->right = NULL;
+		return (NULL);
 	if (parent->n > value)
 		parent->right = new_node;
 	else
