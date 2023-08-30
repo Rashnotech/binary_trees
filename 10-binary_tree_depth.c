f2cd821f9e889c853aa4c15284585685cad12607
Rashnotech <rashnotech@gmail.com>
2023-08-30 12:45:24 +0000

    removed trailing whitespace

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,13 +3,12 @@
  * binary_tree_depth - a function that measures the depth of a node in a
  * binary tree
  * @tree: is pointer to the node to measure the depth
- *
  * Return: depth of tree otherwise 0
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
-	
+
 	if (!tree)
 		return (0);
 	if (!tree->parent)
