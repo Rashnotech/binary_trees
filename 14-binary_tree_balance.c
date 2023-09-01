0dcf702a6cc810aeae48c6ac0f92b0b8c43c77bc
Rashnotech <rashnotech@gmail.com>
2023-09-01 04:46:00 +0000

    removed type cast

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,11 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int _height_r, _height_l;
+	size_t _height_r, _height_l;
 
 	if (!tree)
 		return (0);
 	_height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 	_height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	return ((int)(_height_l - _height_r));
+	return (_height_l - _height_r);
 }
