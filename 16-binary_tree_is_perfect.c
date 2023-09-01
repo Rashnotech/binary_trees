1ff2c5b82c0f80fcc189bc4eaa6ce6d7e40298a0
Rashnotech <rashnotech@gmail.com>
2023-09-01 09:28:16 +0000

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,23 +18,3 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 				binary_tree_is_perfect(tree->right));
 	return (0);
 }
-
-
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree. If tree is NULL, return 0.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-        size_t left_height, right_height;
-
-        if (tree == NULL)
-                return (0);
-
-        left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-        return (left_height > right_height ? left_height : right_height);
-}
